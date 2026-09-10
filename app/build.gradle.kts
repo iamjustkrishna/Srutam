9d70583933842d61e97e8567cda0a3c54382cac3
Krishna Jeena <91063845+QAEYE@users.noreply.github.com>
2026-09-10 20:53:44 +0530

    r8 reduc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "space.iamjustkrishna.srutam"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0.0"
+        versionCode = 5
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
