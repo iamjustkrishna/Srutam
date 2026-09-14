62795291411dc0654d9263fea33518ba077a5405
Krishna Jeena <91063845+QAEYE@users.noreply.github.com>
2026-09-14 23:08:15 +0530

    build: bump versionCode to 6 and versionName to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "space.iamjustkrishna.srutam"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.0.1"
+        versionCode = 6
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
